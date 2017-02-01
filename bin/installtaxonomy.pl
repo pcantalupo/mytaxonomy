3db00ce4eb53359f740acb8bbad61b2b3cdeac98
Paul Cantalupo <pcantalupo@gmail.com>
2017-02-01 11:25:21 -0500

    download accession2taxid files also

--- bin/installtaxonomy.pl
+++ bin/installtaxonomy.pl
@@ -4,16 +4,21 @@ use warnings;
 
 my $base  = "ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/";
 
-my @files = qw/gi_taxid_nucl.dmp.gz
-               gi_taxid_prot.dmp.gz
-               taxdump.tar.gz
-               taxcat.tar.gz/;
+my @files = ('gi_taxid_nucl.dmp.gz',
+             'gi_taxid_prot.dmp.gz',
+             'taxdump.tar.gz',
+             'taxcat.tar.gz',
+             'accession2taxid/nucl_gb.accession2taxid.gz',
+             'accession2taxid/prot.accession2taxid.gz',
+             );
+
                 
 foreach my $file (@files) {
   my $ftpfile = $base . $file;
   print "Getting $file ($ftpfile)\n";
   system ("wget", $ftpfile);
   
+  $file =~ s/\w+\///;   # get the basename
   print "Extracting $file\n";
   if ($file =~ /tar.gz$/) {
     system("tar","xvzf", $file);
