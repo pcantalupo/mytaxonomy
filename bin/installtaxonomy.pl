085288843c2244b81f6757ba2ad730bb9d3206d0
Paul Cantalupo <pcantalupo@gmail.com>
2018-09-24 17:27:23 -0400

    1) not downloading acc2tax files by default. 2) adding support for force and verbose options

--- bin/installtaxonomy.pl
+++ bin/installtaxonomy.pl
@@ -1,31 +1,49 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use Getopt::Long;
 
 my $base  = "ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/";
 
-my @files = ('gi_taxid_nucl.dmp.gz',
-             'gi_taxid_prot.dmp.gz',
-             'taxdump.tar.gz',
-             'taxcat.tar.gz',
-             'accession2taxid/nucl_gb.accession2taxid.gz',
-             'accession2taxid/prot.accession2taxid.gz',
-             );
+my $acc2taxid = 0;
+my $force = 0;
+my $verbose = 0;
+GetOptions("a|accession2taxid" => \$acc2taxid,
+           "f|force" => \$force,
+           "v|verbose" => \$verbose);
 
-                
-foreach my $file (@files) {
-  my $ftpfile = $base . $file;
-  print "Getting $file ($ftpfile)\n";
-  system ("wget", $ftpfile);
-  
-  $file =~ s/\w+\///;   # get the basename
-  print "Extracting $file\n";
-  if ($file =~ /tar.gz$/) {
-    system("tar","xvzf", $file);
-  }
-  else {
-    system ("gunzip", "-f", $file);
-  } 
+my %files = ('gi_taxid_nucl.dmp.gz' => 'gi_taxid_nucl.dmp',
+             'gi_taxid_prot.dmp.gz' => 'gi_taxid_prot.dmp',
+             'taxdump.tar.gz' => 'readme.txt',
+             'taxcat.tar.gz'=> 'categories.dmp');
+
+if ($acc2taxid) {
+  $files{'accession2taxid/nucl_gb.accession2taxid.gz'} = 'nucl_gb.accession2taxid';
+  $files{'accession2taxid/prot.accession2taxid.gz'}    = 'prot.accession2taxid';
 }
 
+#foreach my $file (keys %files) {
+#  print join("\t", $file, $files{$file}), $/;
+#}
+ 
+foreach my $file (keys %files) {
+  if ($force || !-e $files{$file}) {
+    print $files{$file}, " will be downloaded because either it does not exist or force in effect\n" if ($verbose);
 
+    my $ftpfile = $base . $file;
+    print "Getting $file ($ftpfile)\n";
+    system ("wget", $ftpfile);
+    
+    $file =~ s/\w+\///;   # get the basename
+    print "Extracting $file\n";
+    if ($file =~ /tar.gz$/) {
+      system("tar","xvzf", $file);
+    }
+    else {
+      system ("gunzip", "-f", $file);
+    }
+  }
+  else {
+    print $files{$file}, " exists, so skipping download\n" if ($verbose);
+  }
+}
